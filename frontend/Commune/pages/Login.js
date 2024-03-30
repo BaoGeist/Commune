a731c4d40e6cf07be5491a2f7f1d7f512b0b001f
BaoGeist <linofbaoze@outlook.com>
2024-03-30 12:13:13 -0400

    feat: add logo with new theming

--- frontend/Commune/pages/Login.js
+++ frontend/Commune/pages/Login.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { TextInput,TouchableOpacity, Text, View, Button } from 'react-native';
+import { TextInput,TouchableOpacity, Text, View, Image } from 'react-native';
 import React, { useState } from 'react';
 import tw from 'twrnc';
 
@@ -14,26 +14,35 @@ export default function Login() {
 
     return (
         <View style={tw`flex-1 justify-center items-center bg-white`}>
-        <View style={tw`mb-6`}>
-            <TextInput
-            style={tw`bg-blue-100 px-1 border border-blue-500 rounded w-64`}
-            placeholder="Email"
-            placeholderTextColor="#003f5c"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            />
-        </View>
-        <View style={tw`mb-6`}>
-            <TextInput
-            style={tw`bg-blue-100 px-1 border border-blue-500 rounded w-64`}
-            placeholder="Password"
-            placeholderTextColor="#003f5c"
-            secureTextEntry={true}
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            />
-        </View>
-            <TouchableOpacity onPress={handlePress} style={tw`bg-blue-500 px-1 rounded w-32`}>
+            <View style={tw`pb-5`}>
+                <Image
+                    source={require('../assets/logo_with_text.png')}
+                    style={{ width: 200, height: 100 }}
+                />
+            </View>
+            
+            <View style={tw`mb-6`}>
+                <TextInput
+                style={tw`bg-purple-100 px-1 border border-purple-500 rounded w-64`}
+                placeholder="Email"
+                placeholderTextColor="#003f5c"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                />
+            </View>
+
+            <View style={tw`mb-6`}>
+                <TextInput
+                style={tw`bg-purple-100 px-1 border border-purple-500 rounded w-64`}
+                placeholder="Password"
+                placeholderTextColor="#003f5c"
+                secureTextEntry={true}
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                />
+            </View>
+            
+            <TouchableOpacity onPress={handlePress} style={tw`bg-purple-500 px-1 rounded w-32`}>
                 <Text style={tw`text-white text-center font-bold`}>LOGIN</Text>
             </TouchableOpacity>
         </View>
