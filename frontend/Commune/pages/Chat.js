2fe2697de997f88efe5b1d6f35a4bac759150716
BaoGeist <linofbaoze@outlook.com>
2024-03-30 17:51:01 -0400

    fix: added navigation to chat and summarize pages

--- frontend/Commune/pages/Chat.js
+++ frontend/Commune/pages/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Chat = () => {
+const Chat = ({navigation}) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to the Chat Page!</Text>
--- frontend/Commune/pages/Summarize.js
+++ frontend/Commune/pages/Summarize.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const Summarize = () => {
+const Summarize = ({navigation}) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Welcome to the Summarize Page!</Text>
