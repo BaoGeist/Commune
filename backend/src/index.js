5fbf5baaac07ea80807578a3932433311bf5d72b
BaoGeist <linofbaoze@outlook.com>
2024-04-01 04:13:16 -0400

    feat: added signUp and signIn methods to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 // routes
-const loginRouter = require('./routes/loginRouter.js')
+const AccountRouter = require('./routes/accountRouter.js')
 
 app.use(express.json());
-app.use("/", loginRouter)
+app.use("/account", AccountRouter)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
